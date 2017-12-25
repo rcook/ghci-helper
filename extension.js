9f0e6c4bf5e68ecae51dd78dbffbffb88cc0aab2
Richard Cook <rcook@rcook.org>
2017-12-24 17:47:22 -0800

    Implement ghciHelperStart and ghciHelperReload

--- extension.js
+++ extension.js
@@ -5,22 +5,38 @@ const vscode = require('vscode');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
+    var terminal = null;
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "ghci-helper" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let ghciHelperStart = vscode.commands.registerCommand("extension.ghciHelperStart", function() {
+        if (terminal == null) {
+            terminal = vscode.window.createTerminal("GHCi");
+            terminal.show();
+            terminal.sendText("stack ghci");
+        }
+        else {
+            vscode.window.showInformationMessage("GHCi terminal is already started");
+        }
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(ghciHelperStart);
+
+    let ghciHelperReload = vscode.commands.registerCommand("extension.ghciHelperReload", function () {
+        if (terminal == null) {
+            vscode.window.showInformationMessage("No terminal set: start GHCi with \"startGhci\" command");
+        }
+        else {
+            terminal.show();
+            terminal.sendText(":reload");
+            terminal.sendText(":main");
+            vscode.window.showInformationMessage(":reload and :main sent to terminal");
+        }
+    });
+
+    context.subscriptions.push(ghciHelperReload);
 }
 exports.activate = activate;
 
