40a7bde458bd448c7e67db36a3667dc64b8d6597
Richard Cook <rcook@rcook.org>
2017-12-27 06:57:01 -0800

    Remove boilerplate

--- extension.js
+++ extension.js
@@ -1,9 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
     var terminal = null;
 
@@ -15,10 +11,6 @@ function activate(context) {
             terminal.sendText(command);
         }
     }
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "ghci-helper" is now active!');
 
     context.subscriptions.push(vscode.commands.registerCommand("ghciHelper.start", function() {
         if (terminal == null) {
@@ -41,7 +33,6 @@ function activate(context) {
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
 }
 exports.deactivate = deactivate;
